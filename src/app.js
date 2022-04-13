f150da9e29bb93c5885dd8e86f9c69c0c575bc02
Alex Martin <alextravc.342@gmail.com>
2022-04-12 22:43:08 -0500

    [FIX]: build script

---
+++ src/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import express from 'express';
+
+import './db/db';
+import { setupMiddlewares } from './middlewares/setup.middleware';
+import { authRoutes, calendarEventsRoutes } from './routes';
+
+// Initializations:
+const app = express();
+
+// Middlewares
+setupMiddlewares(app);
+
+// Routes
+app.route('/');
+app.use('/api/auth', authRoutes);
+app.use('/api/events', calendarEventsRoutes);
+
+export default app;
